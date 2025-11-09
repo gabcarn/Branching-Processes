a7266750d1d74e5b41ba7886ec41a8d88cd743bf
Gabriel <gabrielcarneiron@gmail.com>
2025-11-08 23:29:34 -0300

    só incluindo simulações

--- Simulações/simulation1.py
+++ Simulações/simulation1.py
@@ -1,23 +1,28 @@
 import numpy as np 
-
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def branching_bernoulli(populacao_atual, quantia_clones, geracoes, probabilidade):
     geracao_atual = populacao_atual
     lista_geraçoes = []
-    lista_geracoes.append(geracao_atual)
+    lista_geraçoes.append(geracao_atual)
 
     for i in range(geracoes):
         acumulado = 0
         for i in range(geracao_atual):
             acumulado += quantia_clones*np.random.binomial(1,probabilidade)
-        lista_geracoes.append(acumulado)
+        lista_geraçoes.append(acumulado)
         geracao_atual = acumulado
         if geracao_atual == 0:
             break
 
-    return lista_geracoes
+    return lista_geraçoes
 
+tamanhos = []
 
+for i in range(500):
+    tamanhos.append(len(branching_bernoulli(2,4,50,0.4)))
 
-
+plt.hist(tamanhos)
+plt.savefig('grafico.png')
 
